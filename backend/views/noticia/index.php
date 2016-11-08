8d4c4ee5ef7531813a6e7be190af57cb4f8e9ba3
Fausto Caicedo <Fausto Caicedo@FaustoCaicedo>
2016-11-08 13:33:29 -0500

    Primer avance casi completo de mi cms

---
+++ backend/views/noticia/index.php
@@ -0,0 +1,41 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel backend\models\NoticiaSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Noticias';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="noticia-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <p>
+        <?= Html::a('Create Noticia', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'titulo',
+            'seo_slug',
+            'detalle',
+            'categoria_id',
+            // 'estado',
+            // 'created_by',
+            // 'created_at',
+            // 'updated_by',
+            // 'updated_at',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+</div>
